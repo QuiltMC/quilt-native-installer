445904495cab4aa3df9a6bd51d1dd881d7355ee6
Glitch <glitch.g3431@gmail.com>
2023-03-05 17:17:00 -0600

    Partial server install support

--- src/installer.rs
+++ src/installer.rs
@@ -1,17 +1,19 @@
 use std::{
     collections::HashMap,
     fs::{self, File},
-    io::{self, Seek},
+    io::{self, Seek, Write},
     path::PathBuf,
 };
 
 use anyhow::{anyhow, Result};
 use base64::{engine::general_purpose::STANDARD as BASE64, Engine};
 use chrono::{DateTime, Utc};
+use iced::futures::future::try_join_all;
 use reqwest::get;
 use semver::Version;
 use serde::{Deserialize, Serialize};
 use serde_json::{Map, Value};
+use zip::CompressionMethod;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Default)]
 pub enum Installation {
@@ -156,31 +158,7 @@ pub async fn install_client(args: ClientInstallation) -> Result<()> {
     .text()
     .await?;
 
-    // Hack-Fix:
-    // Was fixed in versions above 0.17.7
-    if args.loader_version.version < Version::new(0, 17, 7) {
-        // Quilt-meta specifies both hashed and intermediary, but providing both to quilt-loader causes it to silently fail remapping.
-        // This really shouldn't be fixed here in the installer, but we need a solution now.
-        let mut json: Value = serde_json::from_str(&response).unwrap();
-        let libs = json
-            .as_object_mut()
-            .unwrap()
-            .get_mut("libraries")
-            .unwrap()
-            .as_array_mut()
-            .unwrap();
-        libs.retain(|lib| {
-            !lib.as_object()
-                .unwrap()
-                .get("name")
-                .unwrap()
-                .as_str()
-                .unwrap()
-                .starts_with("org.quiltmc:hashed")
-        });
-        response = serde_json::to_string(&json).unwrap();
-    }
-    // End of hack-fix
+    response = hack_fix(response, args.loader_version);
 
     io::copy(&mut response.as_bytes(), &mut file)?;
 
@@ -215,8 +193,127 @@ pub async fn install_client(args: ClientInstallation) -> Result<()> {
     Ok(())
 }
 
+pub fn hack_fix(response: String, loader_version: LoaderVersion) -> String {
+    if loader_version.version < Version::new(0, 17, 7) {
+        // Hack-Fix:
+        // Quilt-meta specifies both hashed and intermediary, but providing both to quilt-loader causes it to silently fail remapping.
+        // This really shouldn't be fixed here in the installer, but we need a solution now.
+        let mut json: Value = serde_json::from_str(&response).unwrap();
+        let libs = json
+            .as_object_mut()
+            .unwrap()
+            .get_mut("libraries")
+            .unwrap()
+            .as_array_mut()
+            .unwrap();
+        libs.retain(|lib| {
+            !lib.as_object()
+                .unwrap()
+                .get("name")
+                .unwrap()
+                .as_str()
+                .unwrap()
+                .starts_with("org.quiltmc:hashed")
+        });
+
+        return serde_json::to_string(&json).unwrap()
+    }
+
+    response
+}
+
 pub async fn install_server(args: ServerInstallation) -> Result<()> {
     println!("Installing server\n{args:#?}");
-    println!("Server installation hasn't been implemented yet!");
-    Err(anyhow!("Server installation hasn't been implemented!"))
+
+    // Download launch json
+    let mut response = get(format!(
+        "https://meta.quiltmc.org/v3/versions/loader/{}/{}/server/json",
+        &args.minecraft_version.version, &args.loader_version.version
+    ))
+        .await?
+        .text()
+        .await?;
+
+    response = hack_fix(response, args.loader_version);
+
+    let json: Value = serde_json::from_str(&response).unwrap();
+    let json = json.as_object().unwrap();
+
+    // first, read libs
+    let libraries = json.get("libraries").unwrap().as_array().unwrap();
+
+    let mut lib_path = args.install_location.clone();
+    lib_path.push("libraries/");
+
+    let library_futures: Vec<_> = libraries.iter().map(|lib| {
+        let lib = lib.as_object().unwrap();
+        let name = lib.get("name").expect("Library had no name").as_str().unwrap().to_string();
+        let maven = lib.get("url").expect("Library had no maven url").as_str().unwrap().to_string();
+
+        download_library(&lib_path, name, maven)
+    }).collect();
+
+    let library_paths: Vec<PathBuf> = try_join_all(library_futures).await?;
+    let mut launch_jar_path = args.install_location.clone();
+    launch_jar_path.push("quilt-server-launch.jar");
+
+    create_launch_jar(launch_jar_path, json.get("launcherMainClass").unwrap().as_str().unwrap().to_string(), library_paths).await?;
+
+
+    Ok(())
+}
+
+async fn download_library(dir: &PathBuf, name: String, maven: String) -> Result<PathBuf> {
+    let response = get(maven_to_url(maven, &name));
+    let mut file_path = dir.clone();
+
+    file_path.push(split_artifact(&name));
+
+    let _ = fs::create_dir_all(file_path.parent().unwrap());
+    let mut file = tokio::fs::File::create(&file_path).await?;
+    let mut content = io::Cursor::new(response.await?.bytes().await?);
+
+    tokio::io::copy(&mut content, &mut file).await?;
+
+    Ok(file_path)
+}
+
+fn maven_to_url(maven_url: String, artifact_notation: &String) -> String {
+    return maven_url + split_artifact(artifact_notation).as_str();
+}
+
+fn split_artifact(artifact_notation: &String) -> String {
+    let parts: Vec<&str> = artifact_notation.splitn(3, ":").collect();
+
+    parts[0].replace('.', "/") + // group
+        "/" + parts[1] + // artifact name
+        "/" + parts[2] + // version
+        "/" + parts[1] +
+        "-" + parts[2] + ".jar"
+}
+
+
+async fn create_launch_jar(path: PathBuf, main_class: String, libraries: Vec<PathBuf>) -> Result<()> {
+    if path.try_exists()? {
+        fs::remove_file(&path).expect("Failed to delete old launch jar");
+    }
+
+    let parent_dir = path.parent().unwrap();
+    let file = File::create(&path)?;
+    let mut zip = zip::ZipWriter::new(file);
+
+    // TODO: there's probably a more efficient way to write this, but does it really matter?
+    let options = zip::write::FileOptions::default().compression_method(CompressionMethod::Stored);
+    zip.start_file("META-INF/MANIFEST.MF", options)?;
+    // create class-path entry
+    let mut cp: String = String::new();
+
+    for x in libraries {
+        cp.push(' ');
+        cp.push_str(x.strip_prefix(parent_dir)?.as_os_str().to_str().unwrap());
+    }
+
+    zip.write_all(format!("Manifest-Version: 1.0\nMain-Class: {main_class}\nClass-Path:{cp}").as_bytes())?;
+    zip.finish()?;
+    Ok(())
 }
